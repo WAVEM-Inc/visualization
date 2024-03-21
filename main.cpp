fc40e79e7cc8ae48044e00376283ec9920005119
antique <jaehyeok718@wavem.net>
2024-03-21 14:59:42 +0900

    feat: update NatSatFixSubscriber

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 #include "ui/main/main_window.h"
 #include "utils/file/code_file_reader.h"
 #include "utils/file/config_file_writer.h"
+#include "ros2/nav_sat_fix_subscriber.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
+    rclcpp::init(argc, argv);
 
+    auto node = std::make_shared<NavSatFixSubscriber>();
+    std::thread ros_thread([&]() {
+        rclcpp::spin(node);
+    });
 
     MainWindow *window = new MainWindow();
     window->resize(800, 600);
@@ -15,7 +21,15 @@ int main(int argc, char *argv[]) {
     QObject::connect(&app, &QApplication::aboutToQuit, []() {
         ConfigFileWriter writer;
         writer.saveFile();
+
+        rclcpp::shutdown();
     });
 
-    return app.exec();
+    int result = app.exec();
+
+    if (ros_thread.joinable()) {
+        ros_thread.join();
+    }
+
+    return result;
 }
--- src/ros2/nav_sat_fix_subscriber.cpp
+++ src/ros2/nav_sat_fix_subscriber.cpp
@@ -7,7 +7,7 @@
 
 NavSatFixSubscriber::NavSatFixSubscriber() : rclcpp::Node("route_editor_navsatfix_subscriber") {
     _subscription_ptr = this->create_subscription<sensor_msgs::msg::NavSatFix>(
-            "?", rclcpp::QoS(rclcpp::KeepLast(10)), std::bind(&NavSatFixSubscriber::topicCallback, this, _1));
+            "sensor/ublox/fix", rclcpp::QoS(rclcpp::KeepLast(10)), std::bind(&NavSatFixSubscriber::topicCallback, this, _1));
 }
 
 void NavSatFixSubscriber::topicCallback(const sensor_msgs::msg::NavSatFix::SharedPtr msg) const {
