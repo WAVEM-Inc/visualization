13003a03c95b41b1117d07a6795865200b1049e0
reidlo5135 <201840103@email.daelim.ac.kr>
2024-06-03 13:49:30 +0900

    fix : error handling of build scripts

--- rmserver/http/script/build.sh
+++ rmserver/http/script/build.sh
@@ -2,8 +2,12 @@
 npm run server_build
 npm run client_build
 
-if ! grep -qxF 'alias rmviz="cd ~/kec_ws/src/service/rmviz/rmserver/web && npm run launch"' ~/.bashrc; then
-    echo 'alias rmviz="cd ~/kec_ws/src/service/rmviz/rmserver/web && npm run launch"' >> ~/.bashrc
+if ! grep -qxF 'alias rmviz="cd ~/kec_ws/src/service/rmviz/rmserver/http && npm run launch"' ~/.bashrc; then
+    echo 'alias rmviz="cd ~/kec_ws/src/service/rmviz/rmserver/http && npm run launch"' >> ~/.bashrc
+fi
+
+if ! grep -qxF 'alias mqtt_broker="sudo ~/RobotData/mqtt/mosquitto.sh"' ~/.bashrc; then
+    echo 'alias mqtt_broker="sudo ~/RobotData/mqtt/mosquitto.sh"' >> ~/.bashrc
 fi
 
 source ~/.bashrc
